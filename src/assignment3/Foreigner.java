564c3a8d569f3babb5da4781275a71edece107be
DoVanThang118 <102956076+DoVanThang118@users.noreply.github.com>
2022-09-29 14:55:50 +0700

    java

--- src/assignment3/Foreigner.java
+++ src/assignment3/Foreigner.java
@@ -1,47 +1,14 @@
 package assignment3;
 
-public class Foreigner {
-    public int id;
-    public String name;
-    public int date;
+public class Foreigner extends Customer {
     public int nationality;
-    public int amount;
-    public int quota;
 
     public Foreigner() {
     }
 
-    public Foreigner(int id, String name, int date, int nationality, int amount, int quota) {
-        this.id = id;
-        this.name = name;
-        this.date = date;
+    public Foreigner(int code, String fullName, String date, int quantity, int nationality) {
+        super(code, fullName, date, quantity);
         this.nationality = nationality;
-        this.amount = amount;
-        this.quota = quota;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getDate() {
-        return date;
-    }
-
-    public void setDate(int date) {
-        this.date = date;
     }
 
     public int getNationality() {
@@ -52,24 +19,8 @@ public class Foreigner {
         this.nationality = nationality;
     }
 
-    public int getAmount() {
-        return amount;
+    @Override
+    public int thanhTien() {
+        return super.thanhTien();
     }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
-
-    public int getQuota() {
-        return quota;
-    }
-
-    public void setQuota(int quota) {
-        this.quota = quota;
-    }
-
-    public int money(){
-        return amount*2000;
-    }
-
 }
--- src/assignment3/Main.java
+++ src/assignment3/Main.java
@@ -4,9 +4,13 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args){
-        Foreigner f1 = new Foreigner();
-        f1.amount = 12;
-        f1.money();
-        System.out.println(f1.money());
+        Vietnamese vietnamese = new Vietnamese(1,"asd","2022-9-22",219,"ca nhan");
+        Foreigner foreigner = new Foreigner(1,"asd","2022-9-22",219,1);
+        System.out.println(vietnamese.thanhTien());
+        System.out.println(foreigner.thanhTien());
+
+
+        Customer customer = (Customer) new Vietnamese(2,"gfr","2022-9-22",219,"ca nhan");
+        System.out.println(customer.thanhTien());
     }
 }
