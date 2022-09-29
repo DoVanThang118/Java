633ef51eb627ad60bad1dbbdbac46ce03e92336e
DoVanThang118 <102956076+DoVanThang118@users.noreply.github.com>
2022-09-29 20:19:55 +0700

    java

--- src/assignment5/Main.java
+++ src/assignment5/Main.java
@@ -4,9 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        INews n = new INews();
-        Scanner sc = new Scanner(System.in);
-        n.RateList = new int[]{9, 8, 10};
-        System.out.println(n.AverageRate);
+
+
     }
 }
